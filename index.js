7093552cbb52ed080d38f76bddf9d54e9c5f5de4
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-09-28 10:25:16 +0200

    doubleaboba

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const atStart = async () => {
   try {
     await mongoConnect();
 
-    aboba();
     return console.log("Connected to MongoDB!");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
