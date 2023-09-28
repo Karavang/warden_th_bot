05d49ac352a72bb051901c8e5c1bba64776f4090
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-09-28 02:16:43 +0200

    sad

--- index.js
+++ index.js
@@ -41,28 +41,24 @@ bot.on("new_chat_members", async (msg) => {
 });
 
 const aboba = async () => {
-  if (chatId) {
-    try {
-      const allUsers = await User.find();
+  try {
+    const allUsers = await User.find();
 
-      const thirtyDaysAgo = new Date();
-      thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+    const thirtyDaysAgo = new Date();
+    thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
 
-      const usersToKick = allUsers.filter(
-        (user) => user.joinDate <= thirtyDaysAgo
-      );
+    const usersToKick = allUsers.filter(
+      (user) => user.joinDate <= thirtyDaysAgo
+    );
 
-      console.log(usersToKick);
+    console.log(usersToKick);
 
-      for (const user of usersToKick) {
-        await bot.banChatMember(user.chat, user.userId);
-        await User.findByIdAndRemove(user._id);
-      }
-    } catch (error) {
-      console.error("Error kicking user:", error);
+    for (const user of usersToKick) {
+      await bot.banChatMember(user.chat, user.userId);
+      await User.findByIdAndRemove(user._id);
     }
-  } else {
-    console.error("chatId and userId are not set.");
+  } catch (error) {
+    console.error("Error kicking user:", error);
   }
 };
 setInterval(aboba, 24 * 60 * 60 * 1000);
